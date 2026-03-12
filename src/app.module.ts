725f6b41789cca0916f6aa4b5a90fc225b20f504
Erick-P-Calauro <erickcalauro@gmail.com>
2026-03-12 15:24:06 -0400

    docs/build: Adding db diagram and configuring typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 import { TransactionsModule } from './transactions/transactions.module';
 import { AuthModule } from './auth/auth.module';
 import { AccountsModule } from './accounts/accounts.module';
@@ -7,7 +9,20 @@ import { AccountsModule } from './accounts/accounts.module';
   imports: [
     TransactionsModule, 
     AuthModule, 
-    AccountsModule
+    AccountsModule,
+    ConfigModule.forRoot({
+      envFilePath: ".env"
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: Number.parseInt(process.env.DB_PORT ?? "5432"),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
   ],
   controllers: [],
   providers: [],
